6917f378679edb9c56700a82f7eb9014a0a0ae43
Anne McCloghry <anne.mccloghry@gmail.com>
2021-01-22 16:35:50 +1100

    reorder users

--- WooliesX.Core/Models/User.cs
+++ WooliesX.Core/Models/User.cs
@@ -3,7 +3,8 @@ namespace WooliesX.Core.Models
 {
     public class User : BaseId<int>
     {
-        public string Token { get; set; }
         public string Name { get; set; }
+
+        public string Token { get; set; }
     }
 }
--- WooliesX.Core/Resources/UserResource.cs
+++ WooliesX.Core/Resources/UserResource.cs
@@ -3,7 +3,8 @@ namespace WooliesX.Core.Resources
 {
     public class UserResource //: BaseResource
     {
-        public string Token { get; set; }
         public string Name { get; set; }
+
+        public string Token { get; set; }
     }
 }
